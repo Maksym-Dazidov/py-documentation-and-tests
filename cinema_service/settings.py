584064df33f18d9c60101ada2756841d7b7d741f
Maksym Dazidov <maksymdazidov@gmail.com>
2026-02-14 22:50:20 +0100

    5

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "drf_spectacular",
     "rest_framework.authtoken",
     "debug_toolbar",
@@ -154,6 +155,10 @@ REST_FRAMEWORK = {
     },
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
     )
 }
 
